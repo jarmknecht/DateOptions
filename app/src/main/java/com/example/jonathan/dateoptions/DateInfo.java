102b82adb0619224c8e1c074eedb24d290058266
Dakoda Richardson <rdakodac@yahoo.com>
2018-10-08 12:27:00 -0600

    Updated DateInfo

--- app/src/main/java/com/example/jonathan/dateoptions/DateInfo.java
+++ app/src/main/java/com/example/jonathan/dateoptions/DateInfo.java
@@ -1,4 +1,40 @@
 package com.example.jonathan.dateoptions;
 
-class DateInfo {
+import android.media.Image;
+
+public class DateInfo
+{
+    public String name;
+    public Image pic;
+    public int rating;
+
+    public void setName(String name)
+    {
+        this.name = name;
+    }
+
+    public String getName()
+    {
+        return name;
+    }
+
+    public void setPic(Image pic)
+    {
+        this.pic = pic;
+    }
+
+    public Image getPic()
+    {
+        return pic;
+    }
+
+    public void setRating(int rating)
+    {
+        this.rating = rating;
+    }
+
+    public int getRating()
+    {
+        return rating;
+    }
 }
