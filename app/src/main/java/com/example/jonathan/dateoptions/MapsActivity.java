9265ed42ea7ba2c43cc7b82252aab80d9d12a4ae
Jonathan Armknecht <jonathan.armknecht@gmail.com>
2018-10-10 12:27:49 -0600

    Changed min zoom in map

--- app/src/main/java/com/example/jonathan/dateoptions/MapsActivity.java
+++ app/src/main/java/com/example/jonathan/dateoptions/MapsActivity.java
@@ -38,9 +38,10 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
 
-        // Add a marker in Sydney and move the camera
+        mMap.setMinZoomPreference(15);
         LatLng getoutgames = new LatLng(40.237622, -111.659088);
-        mMap.addMarker(new MarkerOptions().position(getoutgames).title("Marker in Sydney"));
+        //change marker to be accent color
+        mMap.addMarker(new MarkerOptions().position(getoutgames).title("Getout Games"));
         mMap.moveCamera(CameraUpdateFactory.newLatLng(getoutgames));
     }
 }
