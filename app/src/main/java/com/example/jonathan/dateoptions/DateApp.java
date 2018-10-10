036557225bb4fb677d12330b2c0951f3d2070c46
Dakoda Richardson <rdakodac@yahoo.com>
2018-10-10 12:11:29 -0600

    Updated Ratings

--- app/src/main/java/com/example/jonathan/dateoptions/DateApp.java
+++ app/src/main/java/com/example/jonathan/dateoptions/DateApp.java
@@ -41,12 +41,26 @@ public class DateApp
         Ratings1.add(5);
         Ratings1.add(4);
         Ratings1.add(1);
+        List<Integer> Ratings2 = new ArrayList<Integer>();
+        Ratings2.add(4);
+        Ratings2.add(5);
+        Ratings2.add(5);
+        Ratings2.add(5);
+        Ratings2.add(4);
+        Ratings2.add(5);
+        List<Integer> Ratings3 = new ArrayList<Integer>();
+        Ratings3.add(2);
+        Ratings3.add(1);
+        Ratings3.add(1);
+        Ratings3.add(2);
+        Ratings3.add(2);
+        Ratings3.add(1);
         this.dates.add(new DateInfo("BYU Bowling", R.drawable.byubowling, R.drawable.brickoven, Ratings1));
-        this.dates.add(new DateInfo("BYU Creamery", R.drawable.byucreamery, R.drawable.byumoa, Ratings1));
-        this.dates.add(new DateInfo("Chip Cookies", R.drawable.chipcookies, R.drawable.byustadium, Ratings1));
-        this.dates.add(new DateInfo("Fat Cats", R.drawable.fatcats, R.drawable.citycreek, Ratings1));
+        this.dates.add(new DateInfo("BYU Creamery", R.drawable.byucreamery, R.drawable.byumoa, Ratings3));
+        this.dates.add(new DateInfo("Chip Cookies", R.drawable.chipcookies, R.drawable.byustadium, Ratings2));
+        this.dates.add(new DateInfo("Fat Cats", R.drawable.fatcats, R.drawable.citycreek, Ratings2));
         this.dates.add(new DateInfo("Getout Games", R.drawable.getoutgames, R.drawable.colormemine, Ratings1));
-        this.dates.add(new DateInfo("Laser Assault", R.drawable.laserassault, R.drawable.gallivan_center, Ratings1));
+        this.dates.add(new DateInfo("Laser Assault", R.drawable.laserassault, R.drawable.gallivan_center, Ratings3));
         this.dates.add(new DateInfo("Provo Canyon", R.drawable.provocanyon, R.drawable.hfacexterior, Ratings1));
     }
 }
