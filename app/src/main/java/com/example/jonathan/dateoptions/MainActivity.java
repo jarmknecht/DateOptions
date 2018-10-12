78c137ad9d380352aa627fc6e1b7562629e054fc
Jonathan Armknecht <jonathan.armknecht@gmail.com>
2018-10-12 11:51:30 -0600

    adding menu icons

--- app/src/main/java/com/example/jonathan/dateoptions/MainActivity.java
+++ app/src/main/java/com/example/jonathan/dateoptions/MainActivity.java
@@ -54,7 +54,7 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        //getMenuInflater().inflate(R.menu);
+        getMenuInflater().inflate(R.menu.main_activity_menu, menu);
         return true;
     }
 
