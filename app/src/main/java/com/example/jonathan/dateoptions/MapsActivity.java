5ebafeb316a24efe3c40735aa98e1434d495879e
Jonathan Armknecht <jonathan.armknecht@gmail.com>
2018-10-12 02:26:48 -0600

    made the step size of a rating star .5 and fixed to have the whole card clicked

--- app/src/main/java/com/example/jonathan/dateoptions/MapsActivity.java
+++ app/src/main/java/com/example/jonathan/dateoptions/MapsActivity.java
@@ -55,16 +55,6 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         });
     }
 
-
-    /**
-     * Manipulates the map once available.
-     * This callback is triggered when the map is ready to be used.
-     * This is where we can add markers or lines, add listeners or move the camera. In this case,
-     * we just add a marker near Sydney, Australia.
-     * If Google Play services is not installed on the device, the user will be prompted to install
-     * it inside the SupportMapFragment. This method will only be triggered once the user has
-     * installed Google Play services and returned to the app.
-     */
     @Override
     public void onMapReady(GoogleMap googleMap) {
         int height = 200;
@@ -74,7 +64,7 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         Bitmap smallMarker = Bitmap.createScaledBitmap(bMap, width, height, false);
         mMap = googleMap;
         date = (DateInfo) getIntent().getSerializableExtra("serialize_data");
-        mMap.setMinZoomPreference(15);
+        mMap.setMinZoomPreference(19);
         LatLng location = new LatLng(date.getLatitude(),date.getLongitude());
         mMap.addMarker(new MarkerOptions().position(location).title(date.getName()).icon
                 (BitmapDescriptorFactory.fromBitmap(smallMarker)));
